163abeecc6dbc2c3f34a9695ed8e3a1eae1ea98b
mite404 <ethana2008@gmail.com>
2024-03-31 13:23:22 -0400

    continuing OOP in cs50.

--- oop/student.py
+++ oop/student.py
@@ -1,17 +1,39 @@
 class Student:
-    ...
+    def __init__(self, name, house, patronus):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house} reppin' {self.patronus}"
+
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "🦦"
+            case "Jack Russel Terrier":
+                return "🐕"
+            case _:
+                return "🏑"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print("Expecto Patronum!")
+    print(student.charm())
 
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 
 if __name__ == "__main__":
